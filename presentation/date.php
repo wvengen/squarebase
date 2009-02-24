a1ee434a562e33b189c4d524069c28a9d729664b
wvengen <wvengen@squarebase.org>
2009-02-24 15:29:08 +0000

    proper svn:ignore; add intial presentation for date

---
+++ presentation/date.php
@@ -0,0 +1,36 @@
+<?php
+  function probability_date($field) {
+    return preg_match('/^date\b/', $field['Type']) ? 0.5 : 0;
+  }
+
+  function typename_date($field) {
+    return 'date';
+  }
+
+  function in_desc_date() { return 0; }
+  function in_sort_date() { return 0; }
+  function in_list_date() { return 0; }
+  function in_edit_date() { return 1; }
+
+  function formfield_date($metabasename, $databasename, $field, $value, $readonly) {
+    return html('input', array('type'=>'text', 'class'=>$field['presentation'], 'name'=>"field:$field[fieldname]", 'value'=>swap_date($value), 'readonly'=>$readonly ? 'readonly' : null, 'disabled'=>$readonly ? 'disabled' : null));
+  }
+
+  function formvalue_date($field) {
+    return swap_date(parameter('get', "field:$field[fieldname]"));
+  }
+
+  function cell_date($metabasename, $databasename, $field, $value) {
+    return swap_date($value);
+  }
+
+  function swap_date($value) {
+    if (!preg_match('/^(\d+)-(\d+)-(\d+)$/', $value, $matches))
+      return $value;
+    return "$matches[3]-$matches[2]-$matches[1]";
+  }
+  
+  function css_date() {
+    return '';
+  }
+?>
