1cc1676cc14f3149de8636e4553b39527bc8dc60
fransre <frans@squarebase.org>
2009-04-11 11:40:00 +0000

    added an url presentation

---
+++ presentation/url.php
@@ -0,0 +1,40 @@
+<?php
+  function probability_url($field) {
+    if (!preg_match('@^(var)?char\b@', $field['Type']))
+      return 0;
+    $texts = query('data', "SELECT $field[Field] FROM `$field[Database]`.$field[Table] WHERE $field[Field] IS NOT NULL LIMIT 10");
+    while ($text = mysql_fetch_assoc($texts)) {
+      if (!preg_match('@^https?://@', $text[$field['Field']]))
+        return 0;
+    }
+    return 0.6;
+  }
+
+  function typename_url($field) {
+    return 'url';
+  }
+
+  function in_desc_url($field) { return false; }
+  function in_list_url($field) { return false; }
+  function in_edit_url($field) { return true; }
+
+  function is_sortable_url() { return false; }
+
+  function formfield_url($metabasename, $databasename, $field, $value, $readonly) {
+    return
+      html('input', array('type'=>'text', 'class'=>join(' ', array_clean(array($field['presentation'], $readonly ? 'readonly' : null, $field['nullallowed'] ? null : 'notempty'))), 'name'=>"field:$field[fieldname]", 'id'=>"field:$field[fieldname]", 'value'=>$value, 'readonly'=>$readonly ? 'readonly' : null)).
+      ($value ? externalreference($value, 'link') : '');
+  }
+
+  function formvalue_url($field) {
+    return parameter('get', "field:$field[fieldname]");
+  }
+
+  function list_url($metabasename, $databasename, $field, $value) {
+    return $value;
+  }
+  
+  function css_url() {
+    return ".url { width: 20em; }\n";
+  }
+?>
