22274a89d1dd7423514b9c8ec04efe7e56a07137
fransre <frans@squarebase.org>
2010-01-28 20:40:21 +0000

    officially released under GPLv3

--- inflection.php
+++ inflection.php
@@ -1,4 +1,23 @@
 <?php
+  /*
+    Copyright 2009,2010 Frans Reijnhoudt
+
+    This file is part of Squarebase.
+
+    Squarebase is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Squarebase is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+  */
+
   //rules are in local/<locale>/rules_singular_plural.txt
   //a rule has the form "body/suffixsingular/suffixplural"
   //backrefences are allowed
